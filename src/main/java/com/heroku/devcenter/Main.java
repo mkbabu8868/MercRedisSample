52faacc12b53511f6364694f050188e0a7d28ef8
mqamrudd <mqamrudd@BNGLT-DDCZ1Z2.global.sial.com>
2022-01-25 13:07:43 +0530

    first commit

---
+++ src/main/java/com/heroku/devcenter/Main.java
@@ -0,0 +1,37 @@
+package com.heroku.devcenter;
+
+import java.net.URI;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JedisPool pool;
+
+		try {
+
+			URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+			pool = new JedisPool(new JedisPoolConfig(), redisURI.getHost(), redisURI.getPort(),
+					Protocol.DEFAULT_TIMEOUT, redisURI.getUserInfo().split(":", 2)[1]);
+		} catch (Exception e) {
+			throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var.");
+		}
+
+		Jedis jedis = pool.getResource();
+
+		try {
+			String key = "testKey";
+			String value = "testValue";
+			jedis.set(key, value);
+			System.out.println("Value set into Redis is: " + value);
+			System.out.println("Value retrieved from Redis is: " + jedis.get(key));
+		} finally {
+			pool.returnResource(jedis);
+		}
+	}
+
+}
