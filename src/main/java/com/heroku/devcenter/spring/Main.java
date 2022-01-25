50ca64de4d0de4b57a4366fe4b49ed60f048551c
mqamrudd <mqamrudd@BNGLT-DDCZ1Z2.global.sial.com>
2022-01-25 13:38:13 +0530

    ccc

---
+++ src/main/java/com/heroku/devcenter/spring/Main.java
@@ -0,0 +1,29 @@
+package com.heroku.devcenter.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Jedis;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+		JedisPool pool = ctx.getBean(JedisPool.class);
+		Jedis jedis = pool.getResource();
+
+		try {
+			String key = "testKey";
+			String value = "testValueFromSpring";
+			jedis.set(key, value);
+			System.out.println("Value set into Redis is: " + value);
+			System.out.println("Value retrieved from Redis is: " + jedis.get(key));
+		} finally {
+			pool.returnResource(jedis);
+		}
+
+	}
+}
---
+++ src/main/java/com/heroku/devcenter/spring/SpringConfig.java
@@ -0,0 +1,30 @@
+package com.heroku.devcenter.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SpringConfig {
+
+	@Bean
+	public JedisPool getJedisPool(){
+		
+		try {
+			URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+			return new JedisPool(new JedisPoolConfig(), redisURI.getHost(), redisURI.getPort(),
+					Protocol.DEFAULT_TIMEOUT, redisURI.getUserInfo().split(":", 2)[1]);
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var.");
+			
+		} 
+		
+	}
+}
